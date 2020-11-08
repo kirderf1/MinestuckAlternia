efe79b0ca0583f63fbb92681673d67d2c82ae3d9
kirderf1 <kirderf4@gmail.com>
2020-11-08 09:51:50 +0100

    Set render layers for plants

--- src/main/java/com/apocfarce/minestuck_alternia/ClientProxy.java
+++ src/main/java/com/apocfarce/minestuck_alternia/ClientProxy.java
@@ -9,6 +9,13 @@ import net.minecraft.client.renderer.RenderTypeLookup;
 public class ClientProxy {
 	
 	static void setupBlockRenderLayers() {
+		
+		RenderTypeLookup.setRenderLayer(AlterniaBlocks.pyralLeaves, RenderType.getCutoutMipped());
+		RenderTypeLookup.setRenderLayer(AlterniaBlocks.mirrageLeaves, RenderType.getCutoutMipped());
+		RenderTypeLookup.setRenderLayer(AlterniaBlocks.pyralSapling, RenderType.getCutout());
+		RenderTypeLookup.setRenderLayer(AlterniaBlocks.mirrageSapling, RenderType.getCutout());
+		RenderTypeLookup.setRenderLayer(AlterniaBlocks.pyralGrass, RenderType.getCutout());
+		
 		for(Block glass : AlterniaBlocks.hiveGlass) {
 			if(glass != null)	//Apparently the array has a free spot for mutant-color, which isn't used.
 				RenderTypeLookup.setRenderLayer(glass, RenderType.getTranslucent());
