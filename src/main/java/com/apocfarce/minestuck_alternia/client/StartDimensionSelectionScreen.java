9a9fedcec943b986e9864b23f4222d0a69f42904
kirderf1 <kirderf4@gmail.com>
2020-11-16 19:07:19 +0100

    Dimension selection translation keys

--- src/main/java/com/apocfarce/minestuck_alternia/client/StartDimensionSelectionScreen.java
+++ src/main/java/com/apocfarce/minestuck_alternia/client/StartDimensionSelectionScreen.java
@@ -4,18 +4,22 @@ import com.apocfarce.minestuck_alternia.network.AlterniaPacketHandler;
 import com.apocfarce.minestuck_alternia.network.DimensionSelectionPacket;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.button.Button;
-import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.util.text.TranslationTextComponent;
 
 public class StartDimensionSelectionScreen extends Screen {
 	
+	public static final String TITLE = "start_dim_selection.title", PROMPT = "start_dim_selection.prompt",
+			OVERWORLD = "start_dim_selection.overworld", ALTERNIA = "start_dim_selection.alternia";
+	
 	public StartDimensionSelectionScreen() {
-		super(new StringTextComponent("Start dimension selection"));	//TODO should probably be a proper translation
+		super(new TranslationTextComponent(TITLE));
 	}
 	
 	@Override
 	protected void init() {
-		addButton(new Button(width/2 - 100, height/2 - 10, 200, 20, "Overworld", this::pickOverworld));
-		addButton(new Button(width/2 - 100, height/2 + 10, 200, 20, "Alternia", this::pickAlternia));
+		addButton(new Button(width/2 - 100, height/2 - 10, 200, 20, I18n.format(OVERWORLD), this::pickOverworld));
+		addButton(new Button(width/2 - 100, height/2 + 10, 200, 20, I18n.format(ALTERNIA), this::pickAlternia));
 	}
 	
 	@Override
@@ -23,8 +27,7 @@ public class StartDimensionSelectionScreen extends Screen {
 		renderBackground();
 		
 		super.render(mouseX, mouseY, partialTicks);
-		//TODO translation key
-		drawCenteredString(font, "Select a home dimension", width/2, height/2 - 50, 0xFFFFFF);
+		drawCenteredString(font, I18n.format(PROMPT), width/2, height/2 - 50, 0xFFFFFF);
 	}
 	
 	@Override
