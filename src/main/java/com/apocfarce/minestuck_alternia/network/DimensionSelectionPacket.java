df65547829ba3f41d29ba82db97353481347517f
kirderf1 <kirderf4@gmail.com>
2020-11-16 22:46:22 +0100

    Add player set for selection, and properly get the top block for alternia spawning

--- src/main/java/com/apocfarce/minestuck_alternia/network/DimensionSelectionPacket.java
+++ src/main/java/com/apocfarce/minestuck_alternia/network/DimensionSelectionPacket.java
@@ -1,5 +1,6 @@
 package com.apocfarce.minestuck_alternia.network;
 
+import com.apocfarce.minestuck_alternia.util.EventListener;
 import com.apocfarce.minestuck_alternia.util.PlayerDataHelper;
 import com.apocfarce.minestuck_alternia.util.PortalUtil;
 import com.apocfarce.minestuck_alternia.world.AlterniaDimensions;
@@ -19,15 +20,7 @@ public class DimensionSelectionPacket extends PlayToServerPacket {
 	
 	@Override
 	protected void execute(ServerPlayerEntity player) {
-		//TODO track choices in progress so that people can't cheat by sending this packet to at any time teleport to alternia spawn
-		PlayerDataHelper.setSelectedDimension(player, choseAlternia);
-		if(choseAlternia) {
-			ServerWorld alternia = DimensionManager.getWorld(player.server, AlterniaDimensions.getDimensionType(), true, true);
-			//TODO randomize spawn point slightly like regular spawn, perhaps?
-			//Other idea: spawn the player into an existing hive
-			BlockPos spawnPos = alternia.getSpawnPoint();
-			PortalUtil.teleportEntity(player, alternia, spawnPos.getX() + 0.5, spawnPos.getY(), spawnPos.getZ() + 0.5, 0, 0);
-		}
+		EventListener.handleDimensionSelection(player, choseAlternia);
 	}
 	
 	@Override
--- src/main/java/com/apocfarce/minestuck_alternia/util/EventListener.java
+++ src/main/java/com/apocfarce/minestuck_alternia/util/EventListener.java
@@ -2,20 +2,52 @@ package com.apocfarce.minestuck_alternia.util;
 
 import com.apocfarce.minestuck_alternia.network.AlterniaPacketHandler;
 import com.apocfarce.minestuck_alternia.network.ShouldDoSelectionPacket;
+import com.apocfarce.minestuck_alternia.world.AlterniaDimensions;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.event.server.FMLServerStoppingEvent;
 import net.minecraftforge.fml.network.PacketDistributor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class EventListener {
+	
+	private static final Set<ServerPlayerEntity> playerInSelection = new HashSet<>();
 	
 	@SubscribeEvent
 	public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
 		ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
 		if(PlayerDataHelper.hasNotSelectedDimension(player)) {
-			if(mightBeFirstTimeLogin(player))
+			if(mightBeFirstTimeLogin(player)) {
+				playerInSelection.add(player);
 				AlterniaPacketHandler.INSTANCE.send(PacketDistributor.PLAYER.with(() -> player), new ShouldDoSelectionPacket());
-			else PlayerDataHelper.setSelectedDimension(player, false);
+			} else PlayerDataHelper.setSelectedDimension(player, false);
+		}
+	}
+	
+	@SubscribeEvent
+	public static void onServerStopped(FMLServerStoppingEvent event) {
+		playerInSelection.clear();
+	}
+	
+	public static void handleDimensionSelection(ServerPlayerEntity player, boolean choseAlternia) {
+		if(playerInSelection.contains(player)) {
+			playerInSelection.remove(player);
+			PlayerDataHelper.setSelectedDimension(player, choseAlternia);
+			if(choseAlternia) {
+				ServerWorld alternia = DimensionManager.getWorld(player.server, AlterniaDimensions.getDimensionType(), true, true);
+				//Idea: randomize spawn point slightly like regular spawn, perhaps?
+				//Other idea: spawn the player into an existing hive
+				BlockPos spawnPos = alternia.getHeight(Heightmap.Type.MOTION_BLOCKING, alternia.getSpawnPoint());
+				PortalUtil.teleportEntity(player, alternia, spawnPos.getX() + 0.5, spawnPos.getY(), spawnPos.getZ() + 0.5, 0, 0);
+			}
 		}
 	}
 	
