0d35a647821c83a512a10ecab762e2c0eaba305c
kirderf1 <kirderf4@gmail.com>
2020-11-18 17:10:37 +0100

    Implementation of the hive config with hive type serialization

--- src/main/java/com/apocfarce/minestuck_alternia/world/gen/feature/structure/HiveStructureConfig.java
+++ src/main/java/com/apocfarce/minestuck_alternia/world/gen/feature/structure/HiveStructureConfig.java
@@ -2,22 +2,29 @@ package com.apocfarce.minestuck_alternia.world.gen.feature.structure;
 
 import com.mojang.datafixers.Dynamic;
 import com.mojang.datafixers.types.DynamicOps;
+import net.minecraft.util.WeightedList;
 import net.minecraft.world.gen.feature.IFeatureConfig;
 
 import java.util.Random;
 
 public class HiveStructureConfig implements IFeatureConfig {
-	//TODO this is where we would specify any differences between biomes, such as which hive types that will appear
+	
+	private final WeightedList<HiveType> types;
+	
+	public HiveStructureConfig(WeightedList<HiveType> list) {
+		types = list;
+	}
+	
 	@Override
 	public <T> Dynamic<T> serialize(DynamicOps<T> ops) {
-		return null;
+		return new Dynamic<>(ops, types.func_226310_a_(ops, hiveType -> hiveType.serialize(ops)));
 	}
 	
 	public static <T> HiveStructureConfig deserialize(Dynamic<T> dynamic) {
-		return new HiveStructureConfig();
+		return new HiveStructureConfig(new WeightedList<>(dynamic, HiveType::deserialize));
 	}
 	
 	public HiveType pickType(Random random) {
-		return null;
+		return types.func_226318_b_(random);
 	}
 }
--- src/main/java/com/apocfarce/minestuck_alternia/world/gen/feature/structure/HiveType.java
+++ src/main/java/com/apocfarce/minestuck_alternia/world/gen/feature/structure/HiveType.java
@@ -2,6 +2,8 @@ package com.apocfarce.minestuck_alternia.world.gen.feature.structure;
 
 import com.apocfarce.minestuck_alternia.Item.BloodColor;
 import com.apocfarce.minestuck_alternia.Minestuck_alternia;
+import com.mojang.datafixers.Dynamic;
+import com.mojang.datafixers.types.DynamicOps;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
@@ -9,16 +11,17 @@ import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.gen.feature.structure.StructurePiece;
 import net.minecraft.world.gen.feature.template.TemplateManager;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public final class HiveType {
 	public final ResourceLocation name;
 	public final Heightmap.Type heightmap;
-	public final BloodColor color;
 	public final PieceConstructor constructor;
 	
-	private HiveType(ResourceLocation name, BloodColor color, Heightmap.Type heightmap, PieceConstructor constructor) {
+	private HiveType(ResourceLocation name, Heightmap.Type heightmap, PieceConstructor constructor) {
 		this.name = name;
 		this.heightmap = heightmap;
-		this.color = color;
 		this.constructor = constructor;
 	}
 	
@@ -30,7 +33,24 @@ public final class HiveType {
 		StructurePiece create(TemplateManager templates, ResourceLocation name, BlockPos pos, Rotation rotation);
 	}
 	
+	public <T> Dynamic<T> serialize(DynamicOps<T> ops) {
+		return new Dynamic<>(ops, ops.createString(name.toString()));
+	}
+	
+	public static <T> HiveType deserialize(Dynamic<T> dynamic) {
+		return hiveTypes.get(new ResourceLocation(dynamic.asString(DEFAULT_VALUE.toString())));
+	}
+	
+	private static final ResourceLocation DEFAULT_VALUE = new ResourceLocation(Minestuck_alternia.MOD_ID, "");
+	private static final Map<ResourceLocation, HiveType> hiveTypes = new HashMap<>();
+	
+	private static HiveType register(ResourceLocation name, Heightmap.Type heightmap, PieceConstructor constructor) {
+		HiveType type = new HiveType(name, heightmap, constructor);
+		hiveTypes.put(name, type);
+		return type;
+	}
+	
 	private static HiveType makeStandardHive(String name, BloodColor color, BlockPos spawn, BlockPos floor) {
-		return new HiveType(new ResourceLocation(Minestuck_alternia.MOD_ID, name), color, Heightmap.Type.WORLD_SURFACE_WG, SimpleHiveStructurePiece.with(color, floor, spawn));
+		return register(new ResourceLocation(Minestuck_alternia.MOD_ID, name), Heightmap.Type.WORLD_SURFACE_WG, SimpleHiveStructurePiece.with(color, floor, spawn));
 	}
 }
