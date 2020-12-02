ea016f30a395d17e1542d4d7d4520cc2d43d9f07
Cealvan <Athewmay@Live.com>
2020-12-01 17:27:50 -0700

    fixed something that broke somewhere, that i didn't realize i broke

--- src/main/java/com/apocfarce/minestuck_alternia/world/gen/carver/AlterniaCarvers.java
+++ src/main/java/com/apocfarce/minestuck_alternia/world/gen/carver/AlterniaCarvers.java
@@ -1,6 +1,6 @@
 package com.apocfarce.minestuck_alternia.world.gen.carver;
 
-import com.apocfarce.minestuck_alternia.Minestuck_alternia;
+import com.apocfarce.minestuck_alternia.MinestuckAlternia;
 
 import net.minecraft.world.gen.carver.WorldCarver;
 import net.minecraft.world.gen.feature.ProbabilityConfig;
@@ -10,7 +10,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class AlterniaCarvers {
 
-	public static final DeferredRegister<WorldCarver<?>> REGISTER = DeferredRegister.create(ForgeRegistries.WORLD_CARVERS, Minestuck_alternia.MOD_ID);
+	public static final DeferredRegister<WorldCarver<?>> REGISTER = DeferredRegister.create(ForgeRegistries.WORLD_CARVERS, MinestuckAlternia.MOD_ID);
 	
 	public static final RegistryObject<WorldCarver<ProbabilityConfig>> ALTERNIA_CAVES = REGISTER.register("alternia_caves", () -> new AlterniaCaveCarver(ProbabilityConfig::deserialize, 256));
 	public static final RegistryObject<WorldCarver<ProbabilityConfig>> ALTERNIA_CANYONS = REGISTER.register("alternia_canyons", () -> new AlterniaCanyonCarver(ProbabilityConfig::deserialize));
